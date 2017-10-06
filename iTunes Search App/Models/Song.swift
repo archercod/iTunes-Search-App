72970ae002768fe15812ef3349e49ad877d4d452
archercod <pietrzakmarcin@hotmail.com>
2017-10-06 22:50:01 +0200

    Create Album Model

---
+++ iTunes Search App/Models/Song.swift
@@ -0,0 +1,13 @@
+//
+//  Song.swift
+//  iTunes Search App
+//
+//  Created by Marcin Pietrzak on 06.10.2017.
+//  Copyright © 2017 Marcin Pietrzak. All rights reserved.
+//
+
+import Foundation
+
+struct Song {
+    
+}
