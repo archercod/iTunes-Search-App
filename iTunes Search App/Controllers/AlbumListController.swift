2ecdbc5ce99e20006579a5354345fae7258bec88
archercod <pietrzakmarcin@hotmail.com>
2017-10-07 00:14:08 +0200

    Create AlbumCellViewModel and pass data model in model

--- iTunes Search App/Controllers/AlbumListController.swift
+++ iTunes Search App/Controllers/AlbumListController.swift
@@ -13,11 +13,13 @@ class AlbumListController: UITableViewController {
     private struct Constants {
         static let AlbumCellHeight: CGFloat = 80
     }
+    
+    var artist: Artist!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        self.title = artist.name
     }
     
     // MARK: - Table View Delegate
---
+++ iTunes Search App/ViewModels/AlbumCellViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  AlbumCellViewModel.swift
+//  iTunes Search App
+//
+//  Created by Marcin Pietrzak on 06.10.2017.
+//  Copyright © 2017 Marcin Pietrzak. All rights reserved.
+//
+
+import Foundation
+
+struct AlbumCellViewModel {
+    
+    let title: String
+    let releaseDate: String
+    let genre: String
+}
+
+extension AlbumCellViewModel {
+    
+    init(album: Album) {
+        self.title = album.censoredName
+        self.genre = album.primaryGenre.name
+        
+        let formatter = DateFormatter()
+        formatter.locale = Locale.current
+        
+        formatter.dateFormat = "MMM dd, yyyy"
+        
+        self.releaseDate = formatter.string(from: album.releaseDate)
+        
+    }
+}
