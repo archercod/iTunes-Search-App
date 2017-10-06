cd4552738f03e945263315f0b4fc142c42f22be1
archercod <pietrzakmarcin@hotmail.com>
2017-10-06 23:28:33 +0200

    Add SearchBar and create SearchResultsController. Add updateSearchResults to follow user input and live search in tableView. Create SearchBarDataSource to update tableView when user search

---
+++ iTunes Search App/Controllers/SearchResultsController.swift
@@ -0,0 +1,39 @@
+//
+//  SearchResultsController.swift
+//  iTunes Search App
+//
+//  Created by Marcin Pietrzak on 06.10.2017.
+//  Copyright © 2017 Marcin Pietrzak. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    let dataSource = SearchResultsDataSource()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(SearchResultsController.dismissSearchResultsController))
+        
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = self
+        
+        tableView.dataSource = dataSource
+    }
+
+    @objc func dismissSearchResultsController() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension SearchResultsController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        dataSource.update(with: [Stub.artist])
+        tableView.reloadData()
+    }
+}
---
+++ iTunes Search App/DataSources/SearchResultsDataSource.swift
@@ -0,0 +1,41 @@
+//
+//  SearchResultsDataSource.swift
+//  iTunes Search App
+//
+//  Created by Marcin Pietrzak on 06.10.2017.
+//  Copyright © 2017 Marcin Pietrzak. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsDataSource: NSObject, UITableViewDataSource {
+    
+    private var data = [Artist]()
+    
+    override init() {
+        super.init()
+    }
+    
+    func update(with artists: [Artist]) {
+        data = artists
+    }
+    
+    //MARK: - Data Source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ResultCell", for: indexPath)
+        
+        let artist = data[indexPath.row]
+        cell.textLabel?.text = artist.name
+        
+        return cell
+    }
+}
