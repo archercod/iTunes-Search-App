bf1f58b08dbe71812c84ba0f637f12845fb30321
archercod <pietrzakmarcin@hotmail.com>
2017-10-09 10:04:50 +0200

    Create URL Endpoints for search with iTunesMedia enum

---
+++ iTunes Search App/Models/Endpoints/Endpoint.swift
@@ -0,0 +1,13 @@
+//
+//  Endpoint.swift
+//  iTunes Search App
+//
+//  Created by Marcin Pietrzak on 09.10.2017.
+//  Copyright © 2017 Marcin Pietrzak. All rights reserved.
+//
+
+import Foundation
+
+enum Itunes {
+    case search(type: String, media: ItunesMedia?)
+}
